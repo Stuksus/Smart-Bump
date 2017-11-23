386e43ed6ed87f4298a637656eb634310c610b63
Stuksus <itsoluution@gmail.com>
2017-11-23 21:48:31 +0300

    Add files via upload

--- Smart_Bump.ino
+++ Smart_Bump.ino
@@ -17,14 +17,14 @@ void setup() {
 }
  
 void loop() {
-  if (sonar.ping_cm() == 0){
+  if (sonar.ping_cm() == 0){   
     Serial.println("Nothing");
     
   }
   else{
-      for(int i = 0; i<10; i++){            // запись данных в массивы
-        arrayDistant[i] = sonar.ping_cm(); // запись растояния в массив
-        arrayTime[i] = millis(); // запись времени в массив
+      for(int i = 0; i<10; i++){              
+        arrayDistant[i] = sonar.ping_cm();      // запись растояния в массив
+        arrayTime[i] = millis();      // запись времени в массив
         Serial.print("Diatant number(");
         Serial.print(i); 
         Serial.print(") = ");
@@ -34,28 +34,28 @@ void loop() {
         delay(100);
         }
           for(int j = 0; j < 10; j+=2){
-            int rasTime = arrayTime[j+1] - arrayTime[j]; // получение разности растояния(между пятью прами значений)
-            int rasDist = arrayDistant[j]-arrayDistant[j+1];  // получение разности времени(между пятью прами значений)
-            arrayRasTime[flag] = abs(rasTime);  // запись данных разности в массив (время)
-          arrayRasDist[flag] = abs(rasDist);  // запись данных разности в массив (расстояние)
+            int rasTime = arrayTime[j+1] - arrayTime[j];    // получение разности растояния(между пятью прами значений)
+            int rasDist = arrayDistant[j]-arrayDistant[j+1];      // получение разности времени(между пятью прами значений)
+            arrayRasTime[flag] = abs(rasTime);      // запись данных разности в массив (время)
+          arrayRasDist[flag] = abs(rasDist);      // запись данных разности в массив (расстояние)
           flag++;
           }
           for (int i = 0;i<5; i++){
           Serial.print("Diatant ras number(");
           Serial.print(i); 
           Serial.print(") = ");
-          Serial.println(arrayRasDist[i]);  // вывод данных массива разности (растояние)
+          Serial.println(arrayRasDist[i]);     // вывод данных массива разности (растояние)
           Serial.print("Time ras number(");
           Serial.print(i); 
           Serial.print(") = ");
-          Serial.println(arrayRasTime[i]); // вывод данных массива разности(время)
+          Serial.println(arrayRasTime[i]);  // вывод данных массива разности(время)
        } 
 
-        for ( int i = 0; i < 5 ; i++){
-        double speedCar = arrayRasDist[i]/arrayRasTime[i] ;
-        arraySpeed[i] = abs(speedCar);
-        Serial.print("Speed number (");
-        Serial.print(i);
+        for ( int i = 0; i < 5 ; i++){          //цикл сделан зарание, далее скорости будут превращены в одно среднеарифметичесое значение(еще не не написано)
+        double speedCar = arrayRasDist[i]/arrayRasTime[i] ;     // получение скорости по формуле U = S/t
+        arraySpeed[i] = abs(speedCar);  
+        Serial.print("Speed number (");         // запись полученной скорости в массив
+        Serial.print(i);                  
         Serial.print("):");
         Serial.println(arraySpeed[i]);
         Serial.println();
@@ -65,8 +65,4 @@ void loop() {
   
 }
     
-    
-  /*Serial.print("Ping: ");
-  Serial.print(sonar.ping_cm());
-  Serial.println("cm");*/
 
