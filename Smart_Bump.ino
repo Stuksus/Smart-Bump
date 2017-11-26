9affee732e64486690a5cd450739ed8f8999171e
Stuksus <itsoluution@gmail.com>
2017-11-26 23:59:06 +0300

    Add files via upload

--- Smart_Bump.ino
+++ Smart_Bump.ino
@@ -3,28 +3,31 @@
 #define TRIGGER_PIN  12
 #define ECHO_PIN     11
 #define MAX_DISTANCE 30
-int arrayDistant[10];
-int arrayTime[10];
-int arrayRasDist[5];
-int arrayRasTime[5];
-double arraySpeed[5];
-int flag = 0;
+const int sizeRasArray = 5;
+const int sizeSimpleArray = 10;
+int arrayDistant[sizeSimpleArray];
+long arrayTime[sizeSimpleArray];
+int arrayRasDist[sizeRasArray];
+int arrayRasTime[sizeRasArray];
+double arraySpeed[sizeRasArray];
+int flag;
  
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
  
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(57600);
 }
  
 void loop() {
-  if (sonar.ping_cm() == 0){   
+  if (sonar.ping_cm() == 0){
     Serial.println("Nothing");
     
   }
   else{
-      for(int i = 0; i<10; i++){              
-        arrayDistant[i] = sonar.ping_cm();      // запись растояния в массив
-        arrayTime[i] = millis();      // запись времени в массив
+    flag = 0;
+      for(int i = 0; i<sizeSimpleArray; i++){            // запись данных в массивы
+        arrayDistant[i] = sonar.ping_cm(); // запись растояния в массив (нанометры)
+        arrayTime[i] = millis(); // запись времени в массив
         Serial.print("Diatant number(");
         Serial.print(i); 
         Serial.print(") = ");
@@ -33,36 +36,37 @@ void loop() {
         Serial.println(arrayTime[i]);
         delay(100);
         }
-          for(int j = 0; j < 10; j+=2){
-            int rasTime = arrayTime[j+1] - arrayTime[j];    // получение разности растояния(между пятью прами значений)
-            int rasDist = arrayDistant[j]-arrayDistant[j+1];      // получение разности времени(между пятью прами значений)
-            arrayRasTime[flag] = abs(rasTime);      // запись данных разности в массив (время)
-          arrayRasDist[flag] = abs(rasDist);      // запись данных разности в массив (расстояние)
+          for(int j = 0; j < sizeSimpleArray; j+=2){
+            int rasTime = arrayTime[j+1] - arrayTime[j]; // получение разности растояния(между пятью прами значений)
+            int rasDist = arrayDistant[j]-arrayDistant[j+1];  // получение разности времени(между пятью прами значений)
+            arrayRasTime[flag] = abs(rasTime);  // запись данных разности в массив (время)
+          arrayRasDist[flag] = abs(rasDist);  // запись данных разности в массив (расстояние)
           flag++;
           }
-          for (int i = 0;i<5; i++){
+          for (int i = 0;i < sizeRasArray; i++){
           Serial.print("Diatant ras number(");
           Serial.print(i); 
           Serial.print(") = ");
-          Serial.println(arrayRasDist[i]);     // вывод данных массива разности (растояние)
+          Serial.println(arrayRasDist[i]);  // вывод данных массива разности (растояние)
           Serial.print("Time ras number(");
           Serial.print(i); 
           Serial.print(") = ");
-          Serial.println(arrayRasTime[i]);  // вывод данных массива разности(время)
+          Serial.println(arrayRasTime[i]); // вывод данных массива разности(время)
        } 
 
-        for ( int i = 0; i < 5 ; i++){          //цикл сделан зарание, далее скорости будут превращены в одно среднеарифметичесое значение(еще не не написано)
-        double speedCar = arrayRasDist[i]/arrayRasTime[i] ;     // получение скорости по формуле U = S/t
-        arraySpeed[i] = abs(speedCar);  
-        Serial.print("Speed number (");         // запись полученной скорости в массив
-        Serial.print(i);                  
-        Serial.print("):");
+        for ( int i = 0; i < sizeRasArray; i++){
+        double speedCar = (double)arrayRasDist[i]/arrayRasTime[i] ;
+        arraySpeed[i] = abs(speedCar);
+        Serial.print("Speed number (");
+        Serial.print(i);
+        Serial.print(")");
         Serial.println(arraySpeed[i]);
         Serial.println();
         }
         
   }
-  
+  delay(200);
 }
+    
     
 
